58b25c617fdc844113b57a9c39ec756b88c29a76
SegatoElena <37086282+SegatoElena@users.noreply.github.com>
2020-11-25 21:18:19 +0100

    Add files via upload
    Calculate support with TEP

---
+++ Rule.java
@@ -0,0 +1,223 @@
+import java.math.RoundingMode;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Rule {
+	
+	public List<Righe> xList;
+    public List<Righe> yList;
+    public List<CountY> countList;
+    private int columnNumber;
+    private int numberRow;
+    
+    /*
+     * Costruttore vuoto della classe Rule. Inizializza tutte le variabili.
+     */
+    public Rule() {
+    	xList = new LinkedList<Righe>();
+	    yList = new LinkedList<Righe>();
+	    countList = new LinkedList<CountY>();
+	    columnNumber = 0;
+	    numberRow = 0;
+    }
+    
+    public void SetXList(Righe tmp) {
+    	xList.add(tmp);
+    }
+    
+    public Righe GetXList(int index) {    	
+    	return xList.get(index);
+    }
+    
+    public void SetYList(Righe tmp) {
+    	yList.add(tmp);
+    }
+    
+    public int YListSize() {
+    	return yList.size();
+    }   
+    
+    public Righe GetYList(int index) {
+    	return yList.get(index);
+    }
+    
+    public void SetColumn(int value) {
+    	columnNumber = value;
+    }
+    
+    public int GetColumn() {
+    	return columnNumber;
+    }
+    
+    public void SetRow(int value) {
+    	numberRow = value;
+    }
+    
+    public int GetRow() {
+    	return numberRow;
+    }
+	/*
+	 * Metodo che controlla che la colonna di output(yColumn) non sia utilizzata anche come valore di input.
+	 * columnNumber: 		numero di colonne della tabella
+	 * yColumn: 			nome del valore che vogliamo in output
+	 * x: 					lista che contiene i nomi delle colonne da usare come input
+	 * 
+	 * return: 				true se l'elemento yColumn è presente nella lista x
+	 * 						false altrimenti
+	 */
+	public boolean checkElement(String yColumn, LinkedList<String> x) {
+		for(int i = 0; i < GetColumn()-1; i++) {
+	    	if(yColumn.equals(x.get(i))) {
+	    		return true;
+	    	}
+	    }
+		return false;
+	}
+	
+	/*
+	 * Metodo popola le liste per il calcolo della support e della confidence
+	 * resultSet: contiene il risultato della query sottoposta al database
+	 * 
+	 * return: void
+	 */
+	public void XYList(ResultSet resultSet) throws SQLException {
+		Righe tmp;
+		ResultSetMetaData rsmd = resultSet.getMetaData();
+		do  {
+			tmp = new Righe();
+			for(int j = 1; j <= GetColumn(); j++) 
+				tmp.set(resultSet.getString(j));
+			for (int i = 1; i <= GetColumn()+1; i++) {
+		        if (i > 1) System.out.print(",  ");
+		        String columnValue = resultSet.getString(i);
+		        System.out.print(columnValue + " " + rsmd.getColumnName(i));
+		    }
+		    System.out.println("");
+			SetXList(tmp);
+			SetYList(new Righe(resultSet.getString(GetColumn())));
+		} while(resultSet.next());
+	}
+	
+	/*
+	 * Metodo per la stampa dei risultati ottenuti.
+	 * resultSet: contiene il risultato della query sottoposta al database
+	 * index: indice della riga xList a cui i risultati fanno riferimento.
+	 * 
+	 * return: void
+	 */
+	public void PrintResults(ResultSet resultSet, int index) throws SQLException {
+		DecimalFormat df = new DecimalFormat("##.##");
+		df.setRoundingMode(RoundingMode.DOWN);
+		String supp = df.format(((Integer.parseInt(GetYList(index).toString().replace(" ", "")) / (float)GetRow() ) * 100.0));
+		String conf = df.format(((Integer.parseInt(resultSet.getString(2)) / (float)(Integer.parseInt(GetYList(index).toString().replace(" ", "")))) * 100.0));
+		System.out.println(GetXList(index).toString() + " -> " + resultSet.getString(1) + " | "
+			+ supp + "%" 
+			+ " | " + conf + "%" );
+	}
+	/*
+	 * Metodo per la stampa della sola confidence
+	 * resultSet: contiene il risultato della query sottoposta al database
+	 * index: indice della riga xList a cui i risultati fanno riferimento.
+	 * 
+	 * return: void
+	 */
+	public void PrintConfidence(ResultSet resultSet, int index) throws SQLException {
+		DecimalFormat df = new DecimalFormat("##.##");
+		df.setRoundingMode(RoundingMode.DOWN);
+		String conf = df.format(((Integer.parseInt(resultSet.getString(2)) / (float)(Integer.parseInt(GetYList(index).toString().replace(" ", "")))) * 100.0));
+		System.out.println(GetXList(index).toString() + " -> " + resultSet.getString(1) 
+			+ " | " + conf + "%" );
+	}
+	
+	/*
+	 * Metodo per stampare i valori della support
+	 * 
+	 * return: void
+	 */
+	public void printSupport(DBConnector connection) {
+		System.out.println("ID  | SUPPORT (%) ");
+		DecimalFormat df = new DecimalFormat("##.##");
+		df.setRoundingMode(RoundingMode.DOWN);
+		for(int index = 0; index < yList.size()-1; index++)  {
+			String supp = df.format(((Integer.parseInt(GetYList(index).toString().replace(" ", "")) / (float)GetRow() ) * 100.0));
+			System.out.println(convertIDIntoString(GetXList(index).toString(), connection) + " | " + supp + "%");
+		}
+	}
+	
+	public String convertIDIntoString(String id, DBConnector connection) {
+		String query = "SELECT description "
+				+ "FROM trend_event_patterns "
+				+ "WHERE tep_id = " + id + ";";
+		try {
+			ResultSet resultset = connection.DBReplay(query);
+			if (resultset.next())
+				return resultset.getString(1).replace("_", " - ");
+		} catch (SQLException e) {
+			System.out.println("Error during database interrogation" + e.toString());
+		}
+		return "";
+	}
+	
+	/*
+	 * Metodo per la generazione della query per il calcolo della support
+	 * x: lista contenente i valori di input
+	 * return:
+	 * selectSql: query ottenuta dalla combinazione dei valori ricevuti in input
+	 */
+	public String QueryGenerator(String[] x, double param) {
+		String selectSql = "SELECT "; 
+		selectSql += Arrays.toString(x).replace("[", "").replace("]", "") + ", COUNT(*)";	
+		selectSql += " FROM public.icustay_tep GROUP BY " + Arrays.toString(x).replace("[", "").replace("]", "")	
+				+ " HAVING COUNT(*) > (" + GetRow()*param + ");";	        
+
+		return selectSql;
+	}
+	
+	public String QueryGeneratorWithTepid(double param) {
+		String selectSql = "SELECT tep_id, COUNT(*)";	
+		selectSql += " FROM public.icustay_tep "
+				+ "GROUP BY tep_id"
+				+ " HAVING COUNT(*) > (" + GetRow()*param + ");";	        
+
+		return selectSql;
+	}
+	
+	/* 
+	 * Metodo per la generazione della query per il calcolo della confidence
+	 * x: lista contenente i valori di input
+	 * y: lista contentente la y per il calcolo della confidence
+	 * values: tutti i valori che le x possono assumere
+	 * total: numero totale di x con un valore(values) all'iterno del database
+	 * 
+	 * return:
+	 * selectSql: query ottenuta dalla combinazione dei valori ricevuti in input
+	 */
+	public String QueryGenerator(String[] x, String y, Righe values, String total, double param) {
+		String selectSql = "SELECT "; 
+		selectSql += y.replace("[", "").replace("]", "") + ", COUNT(*)";	
+		selectSql += " FROM public.icustay_tep" ;
+				
+		String tmp = " WHERE ";
+		
+		tmp += Arrays.toString(x).replace("[", "");
+				
+		for (int i = 0; i < values.riga.size(); i++) {
+			tmp = tmp.replaceFirst(",", " = '" + values.get().get(i) + "' AND ");
+		}
+				
+		tmp = tmp.replace("]", "= '" + values.get().get(values.get().size()-1))
+			+ "' GROUP BY " + y.replace("[", "").replace("]", "")	
+			+ " HAVING COUNT(*) > (" + Integer.parseInt(total)*param + ")"
+			+ " ORDER BY COUNT(*) DESC;";
+		
+		selectSql += tmp;
+		
+		return selectSql;
+	}
+}
---
+++ main.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class main {
+	/*
+	 * Funzione principale che si interfaccia con la classe DBConnector per comunicare con il database,
+	 * con la classe DataInput per gestire gli input dell'utente, con la classe Rule per il calcolo della support e della confidence
+	 * ed infine con la classe Phase che gestisce tutte le fasi dall'algoritmo.
+	 */
+	public static void main(String[] args) {
+		//Inizializzo gli oggetti
+		Rule rule = new Rule();
+	    DBConnector connection;
+	    DataInput dataInput = new DataInput();
+	    Phases phases = new Phases();
+	    
+		try {
+			//////////////////////////////////////////////////////////////
+			// Inizializzo la connessione con il database
+			connection = phases.connection();
+			
+			// Richiamo il metodo per gestire la fase di input, da linea di comando, dei parametri
+			// necessari per svolgere i vari calcoli
+			phases.insertParameters(rule, dataInput, connection);
+			
+			System.out.println("> ELABORATION ");
+			
+			//////////////////////////////////////////////////////////////
+			// Calcolo delle righe totali all'iterno del DB
+			phases.getTotalRowsOfDatabase(rule, connection);
+			
+			// Gestione delle fasi da eseguire a seconda della scelta effettuata dall'utente,
+			// durante la fasi di inserimento dei parametri
+			switch (dataInput.getCalc()) {
+			case 1:
+				phases.supportCalc(rule, dataInput, connection);
+				phases.confidenceCalc(rule, dataInput, connection);
+				//phases.createTableTep(connection);
+				//phases.insertTepIntoDB(connection, rule);
+				break;
+			case 2:
+				phases.supportCalc(rule, dataInput, connection);
+				//phases.createTableTep(connection);
+				//phases.insertTepIntoDB(connection, rule);
+				break;
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + dataInput.getCalc());
+			}
+
+		} catch (SQLException e1) {
+			connection = null;
+			e1.printStackTrace();
+ 	        System.out.println(e1);
+			System.exit(0);
+		} catch (IOException e2) {
+			System.out.print("Error occurred during inputs phase: " + e2.toString());
+    		System.exit(0);
+		}
+	}
+}
